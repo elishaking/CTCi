9f9c308c474ed5ac4852932e77424724b9cb6f94
King Elisha <ek.chibueze@gmail.com>
2020-06-16 14:52:16 +0100

    implemented string_compression

---
+++ chapter_1/1_6_string_compression/string_compression.py
@@ -0,0 +1,21 @@
+def string_compression(s: str):
+    new_str = ''
+    count = 1
+
+    for i in range(1, len(s)):
+        if s[i] == s[i-1]:
+            count += 1
+        else:
+            new_str += s[i-1] + str(count)
+            print(s[i-1] + str(count))
+            count = 1
+
+    new_str += s[len(s) - 1] + str(count)
+
+    if len(new_str) >= len(s):
+        return s
+
+    return new_str
+
+
+print(string_compression("ddsss"))
