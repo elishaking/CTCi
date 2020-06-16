8fbfa7f7b6c9fd8acdd59a1f476e90d4cbf8acb7
King Elisha <ek.chibueze@gmail.com>
2020-06-16 13:37:25 +0100

    implemented check_permutation

---
+++ chapter_1/1_2_check_permutation/check_permutation.py
@@ -0,0 +1,23 @@
+def char_sum(s: str):
+    sum = 0
+    for char in s:
+        sum += ord(char)
+
+    return sum
+
+
+# Time complexity: O(N)
+# Space complexity: O(1)
+def check_permutation(s1: str, s2: str):
+    sum1 = char_sum(s1)
+    sum2 = char_sum(s2)
+
+    if sum1 == sum2:
+        return True
+
+    return False
+
+
+print(check_permutation("same", "same"))
+print(check_permutation("same", "smae"))
+print(check_permutation("same", "not same"))
