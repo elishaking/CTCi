d3359e517ce481ee9ce6f973449abe845b0023e5
King Elisha <ek.chibueze@gmail.com>
2020-06-17 16:38:40 +0100

    implemented loop_detection

---
+++ chapter_2/2_8_loop_detection.py
@@ -0,0 +1,28 @@
+from linked_list import LinkedList, Node
+
+
+def loop_detection(c_list: LinkedList):
+    lookup = {}
+    current_node = c_list.head
+
+    while current_node:
+        node_id = id(current_node)
+        if lookup.get(node_id):
+            return lookup[node_id]
+        else:
+            lookup[node_id] = current_node
+
+        current_node = current_node.next
+
+    return
+
+
+if __name__ == "__main__":
+    node_1 = Node(10)
+    node_2 = Node(30, next=node_1)
+    l1 = LinkedList().push(10).push(None, node_1).push(50).push(None, node_2)
+    l2 = LinkedList().push(None, node_1).push(
+        100).push(300).push(None, node_2)
+
+    print(loop_detection(l1).value)
+    print(loop_detection(l2).value)
