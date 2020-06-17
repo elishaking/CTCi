a331b9da91ab638307d97e0d49ff648db337d991
King Elisha <ek.chibueze@gmail.com>
2020-06-17 15:49:58 +0100

    implemented palindrome

---
+++ chapter_2/2_6_palindrome.py
@@ -0,0 +1,29 @@
+import math
+from linked_list import LinkedList, Node
+
+
+def palindrome(s_list: LinkedList):
+    chars = []
+    current_node = s_list.head
+
+    while current_node:
+        chars.append(current_node.value)
+        current_node = current_node.next
+
+    left = 0
+    right = len(chars) - 1
+    while right > left:
+        if chars[right] != chars[left]:
+            return False
+
+        right -= 1
+        left += 1
+
+    return True
+
+
+if __name__ == "__main__":
+    s1 = LinkedList().push('l').push('e').push('v').push('e').push('l')
+    s2 = LinkedList().push('l').push('e').push('v').push('f').push('l')
+    print(palindrome(s1))
+    print(palindrome(s2))
