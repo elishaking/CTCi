41505bc42006170cc45f6d54ff4a19bf1dc7782b
King Elisha <ek.chibueze@gmail.com>
2020-06-18 14:42:39 +0100

    implemented my_queue

---
+++ chapter_3/3_4_queue_via_stacks.py
@@ -0,0 +1,30 @@
+from stack import Stack
+
+
+class MyQueue:
+    def __init__(self):
+        self.stack_1 = Stack()
+        self.stack_2 = Stack()
+
+    def add(self, value=0):
+        self.stack_1.push(value)
+
+        return self
+
+    def remove(self):
+        if self.stack_2.is_empty():
+            while not self.stack_1.is_empty():
+                self.stack_2.push(self.stack_1.pop())
+
+            return self.stack_2.pop()
+
+        else:
+            return self.stack_2.pop()
+
+
+if __name__ == "__main__":
+    q = MyQueue().add(10).add(11).add(12).add(13).add(14).add(15)
+
+    print(q.remove())
+    print(q.remove())
+    print(q.remove())
