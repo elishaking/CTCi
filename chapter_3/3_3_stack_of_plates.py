f49e05c88586f34d9b23588f4b8bbb76eb78b598
King Elisha <ek.chibueze@gmail.com>
2020-06-18 14:00:45 +0100

    fist implementation

---
+++ chapter_3/3_3_stack_of_plates.py
@@ -0,0 +1,49 @@
+class SetOfStacks:
+    def __init__(self, max=3):
+        self.max = max
+        self.values = [[]]
+        self.current_stack = 0
+
+    def push(self, value=0):
+        if len(self.values[self.current_stack]) >= self.max:
+            self.current_stack += 1
+            self.values.append([])
+
+        self.values[self.current_stack].append(value)
+
+        return self
+
+    def pop(self):
+        if len(self.values[self.current_stack]) == 0:
+            self.current_stack -= 1
+            self.values.pop()
+
+            if len(self.values) == 0:
+                raise Exception('Stack is empty')
+
+        return self.values[self.current_stack].pop()
+
+    def pop_at(self, index):
+        if index >= len(self.values):
+            raise Exception('Sub stack at index: ' +
+                            str(index) + ' does not exist')
+
+        value = self.values[index].pop()
+
+        if len(self.values[index]) == 0:
+            self.values.pop(index)
+            self.current_stack -= 1
+
+        return value
+
+    def __str__(self):
+        return str(self.values)
+
+
+if __name__ == "__main__":
+    stacks = SetOfStacks(2)
+    stacks.push(10).push(3).push(9).push(15).push(14).push(11).push(100)
+    print(stacks)
+    stacks.pop_at(1)
+    print(stacks)
+    stacks.pop_at(1)
