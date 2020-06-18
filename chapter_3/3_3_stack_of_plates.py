2b2a96b1889da337a40b53f30e23273d5ceaada0
King Elisha <ek.chibueze@gmail.com>
2020-06-18 14:17:11 +0100

    second implementation

--- chapter_3/3_3_stack_of_plates.py
+++ chapter_3/3_3_stack_of_plates.py
@@ -1,6 +1,49 @@
 class SetOfStacks:
     def __init__(self, max=3):
         self.max = max
+        self.values = []
+
+    def push(self, value=0):
+        self.values.append(value)
+
+        return self
+
+    def pop(self):
+        if len(self.values) == 0:
+            raise Exception('Stack is empty')
+
+        return self.values.pop()
+
+    def pop_at(self, index):
+        start_index = index * self.max
+        if start_index >= len(self.values):
+            raise Exception('Sub stack at index: ' +
+                            str(index) + ' does not exist')
+
+        pop_index = start_index + self.max - 1
+        if pop_index > len(self.values):
+            pop_index = len(self.values) - 1
+
+        value = self.values.pop(pop_index)
+
+        return value
+
+    def __str__(self):
+        return str(self.values)
+
+
+if __name__ == "__main__":
+    stacks = SetOfStacks(2)
+    stacks.push(10).push(3).push(9).push(15).push(14).push(11).push(100)
+    print(stacks)
+    stacks.pop_at(1)
+    stacks.pop_at(1)
+    print(stacks)
+
+
+class SetOfStacks_2:
+    def __init__(self, max=3):
+        self.max = max
         self.values = [[]]
         self.current_stack = 0
 
@@ -38,12 +81,3 @@ class SetOfStacks:
 
     def __str__(self):
         return str(self.values)
-
-
-if __name__ == "__main__":
-    stacks = SetOfStacks(2)
-    stacks.push(10).push(3).push(9).push(15).push(14).push(11).push(100)
-    print(stacks)
-    stacks.pop_at(1)
-    print(stacks)
-    stacks.pop_at(1)
