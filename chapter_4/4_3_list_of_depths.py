2743e5de8298a87e8ddf3e044b0349c996b16d4f
King Elisha <ek.chibueze@gmail.com>
2020-06-20 18:14:01 +0100

    implemented list_of_depths

---
+++ chapter_4/4_3_list_of_depths.py
@@ -0,0 +1,30 @@
+from queue import Queue, LinkedList
+from graph import BinarySearchTree
+
+
+# Time complexity: O(N)
+# Space complexity: O(N)
+def list_of_depths(bt: BinarySearchTree):
+    nodes = {}
+    que = Queue()
+    que.add([bt.root, 1])
+
+    while not que.is_empty():
+        (node, pos) = que.remove().value
+
+        if not nodes.get(pos):
+            nodes[pos] = LinkedList()
+
+        nodes[pos].unshift(node.value)
+
+        if node.left:
+            que.add([node.left, pos + 1])
+        if node.right:
+            que.add([node.right, pos + 1])
+
+    return nodes
+
+
+if __name__ == "__main__":
+    bst = BinarySearchTree(2).insert(1).insert(3).insert(5).insert(2)
+    print(list_of_depths(bst))
